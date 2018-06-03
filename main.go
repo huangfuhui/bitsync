7377e96ce68b5e17dfeac70c5c2d4f3e0e39d46f
huangfuhui <huangfuhui@outlook.com>
2018-06-03 13:32:03 +0800

    添加日志配置

--- main.go
+++ main.go
@@ -4,8 +4,21 @@ import (
 	"github.com/astaxie/beego"
 	_ "bitsync/models"
 	_ "bitsync/routers"
+	"github.com/astaxie/beego/logs"
 )
 
+func init() {
+	runMode := beego.AppConfig.String("runmode")
+
+	if runMode == "prod" {
+		beego.SetLevel(beego.LevelInformational)
+	}
+
+	beego.SetLogFuncCall(true)
+	beego.SetLogger(logs.AdapterFile, `{"filename":"log/bitsync.log","daily":true,"maxdays":7}`)
+}
+
 func main() {
+	beego.Info("初始化完成,启动应用...")
 	beego.Run()
 }
