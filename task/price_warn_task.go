3d6755031382b0067a865d3b1805ec5e73d64481
huangfuhui <huangfuhui@outlook.com>
2018-06-18 00:21:10 +0800

    多个号码提醒

--- task/price_warn_task.go
+++ task/price_warn_task.go
@@ -35,25 +35,30 @@ func (task *WarnTask) Warn() {
 			continue
 		}
 
+		phones := []string{"13710227972"}
 		if newPrice+0.5 <= oldPrice {
 			updatePrice := strconv.FormatFloat(oldPrice-0.5, 'f', 1, 64)
 			redis.Set("13710227972:eosusdt", updatePrice)
 			beego.Info("【价格提醒】eos/usdt:" + updatePrice)
 
-			sms := services.SmsService{}
-			err := sms.SendSingle("86", "13710227972", []string{"eos/usdt", updatePrice})
-			if err != nil {
-				beego.Error(err)
+			for _, v := range phones {
+				sms := services.SmsService{}
+				err := sms.SendSingle("86", v, []string{"eos/usdt", updatePrice})
+				if err != nil {
+					beego.Error(err)
+				}
 			}
 		} else if newPrice-0.5 >= oldPrice {
 			updatePrice := strconv.FormatFloat(oldPrice+0.5, 'f', 1, 64)
 			redis.Set("13710227972:eosusdt", updatePrice)
 			beego.Info("【价格提醒】eos/usdt:" + updatePrice)
 
-			sms := services.SmsService{}
-			err := sms.SendSingle("86", "13710227972", []string{"eos/usdt", updatePrice})
-			if err != nil {
-				beego.Error(err)
+			for _, v := range phones {
+				sms := services.SmsService{}
+				err := sms.SendSingle("86", v, []string{"eos/usdt", updatePrice})
+				if err != nil {
+					beego.Error(err)
+				}
 			}
 		}
 	}
