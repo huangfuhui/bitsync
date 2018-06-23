82eb6d1066ea4c335e4fba3daf83cbd464d2edd8
huangfuhui <huangfuhui@outlook.com>
2018-06-23 22:57:45 +0800

    添加响应方法

--- controllers/base_controller.go
+++ controllers/base_controller.go
@@ -1,7 +1,27 @@
 package controllers
 
-import "github.com/astaxie/beego"
+import (
+	"github.com/astaxie/beego"
+	"net/http"
+)
+
+type response struct {
+	code     int
+	response interface{}
+	msg      string
+}
 
 type BaseController struct {
 	beego.Controller
+}
+
+// 正常响应输出JSON数据
+func (c *BaseController) output(data interface{}) {
+	responseData := new(response)
+	responseData.code = http.StatusOK
+	responseData.response = data
+	responseData.msg = ""
+
+	c.Data["json"] = responseData
+	c.ServeJSONP()
 }
