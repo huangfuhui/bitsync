d292dbd833370a3fea8e876f2d8e67af928dcda2
huangfuhui <huangfuhui@outlook.com>
2018-06-26 12:02:11 +0800

    验证密码强度

--- logic/account/account_logic.go
+++ logic/account/account_logic.go
@@ -1,6 +1,9 @@
 package account
 
-import "bitsync/logic"
+import (
+	"bitsync/logic"
+	"regexp"
+)
 
 type AccountLogic struct {
 	logic.BaseLogic
@@ -8,5 +11,17 @@ type AccountLogic struct {
 
 // 注册
 func (l *AccountLogic) Register(handset, password, pin string) {
-
+	match := false
+	match, _ = regexp.MatchString("^.{8,15}$", password)
+	if !match {
+		l.Warn("密码长度必须是8-15个字符")
+	}
+	match, _ = regexp.MatchString("^.*[a-zA-Z].*$", password)
+	if !match {
+		l.Warn("密码至少包含一个字母")
+	}
+	match, _ = regexp.MatchString("^.*[0-9].*$", password)
+	if !match {
+		l.Warn("密码至少包含一个数字")
+	}
 }
--- logic/base_logics.go
+++ logic/base_logics.go
@@ -2,15 +2,8 @@ package logic
 
 import (
 	"bitsync/controllers"
-	"bitsync/error"
 )
 
 type BaseLogic struct {
 	controllers.BaseController
-	error.ApiError
-}
-
-// 获取参数
-func (logic *BaseLogic) Get(param string) string {
-	return logic.GetString(param)
 }
