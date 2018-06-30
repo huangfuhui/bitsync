9db67c1d592f4a74fd0d233790651a4c1d288235
huangfuhui <huangfuhui@meimeifa.com>
2018-06-30 16:46:20 +0800

    会员

---
+++ controllers/account/member_controller.go
@@ -0,0 +1,17 @@
+package account
+
+import "bitsync/controllers"
+
+type MemberController struct {
+	controllers.BaseController
+}
+
+// 获取会员信息
+func (c *MemberController) Get() {
+
+}
+
+// 更新会员信息
+func (c *MemberController) Update() {
+
+}
