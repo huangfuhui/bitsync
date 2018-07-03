2672ac119366e4d60f7738d9856744e376e7fdbf
huangfuhui <huangfuhui@outlook.com>
2018-07-03 15:12:50 +0800

    短信相关表结构

--- object/bitsync.sql
+++ object/bitsync.sql
@@ -1,6 +1,15 @@
 -- 建立数据库
 create database if not exists bitsync default CHARSET utf8 collate utf8_general_ci;
 
+-- 模板
+create table table_name (
+  id int(10) unsigned not null auto_increment,
+  uid int(10) unsigned unique not null comment '用户UID',
+  created_at timestamp null default null,
+  updated_at timestamp null default null,
+  primary key (id)
+) engine=InnoDB default charset=utf8 comment='';
+
 create table account (
   id int(10) unsigned not null auto_increment,
   uid int(10) unsigned unique not null comment '用户UID',
@@ -32,3 +41,61 @@ create table member (
   updated_at timestamp null default null,
   primary key (id)
 ) engine=InnoDB default charset=utf8 comment='会员';
+
+create table sms_wallet (
+  id int(10) unsigned not null auto_increment,
+  uid int(10) unsigned unique not null comment '用户UID',
+  balance int(10) unsigned not null default '0' comment '余额',
+  prepare_consume int(10) unsigned not null default '0' comment '预消费',
+  created_at timestamp null default null,
+  updated_at timestamp null default null,
+  primary key (id)
+) engine=InnoDB default charset=utf8 comment='短信钱包';
+
+create table sms_consume_record (
+  id int(10) unsigned not null auto_increment,
+  uid int(10) unsigned unique not null comment '用户UID',
+  handset varchar(20) not null comment '手机号码',
+  amount int(10) unsigned not null comment '消费条数',
+  sms_content text not null comment '短信内容',
+  consume_time timestamp not null comment '消费时间',
+  status tinyint(3) unsigned not null default '1' comment '消费状态 [1.消费成功 2.消费失败]',
+  created_at timestamp null default null,
+  updated_at timestamp null default null,
+  primary key (id)
+) engine=InnoDB default charset=utf8 comment='短信消费记录';
+
+create table sms_task (
+  id int(10) unsigned not null auto_increment,
+  uid int(10) unsigned unique not null comment '用户UID',
+  task_id int(10) unsigned not null comment '任务ID',
+  type tinyint(3) unsigned not null comment '任务类型 [1.阈值提醒]',
+  status tinyint(3) unsigned not null default '0' comment '任务状态 [0.等待 1.成功 2.失败 3.取消]',
+  finish_time timestamp null default null comment '任务完成时间',
+  created_at timestamp null default null,
+  updated_at timestamp null default null,
+  primary key (id)
+) engine=InnoDB default charset=utf8 comment='短信任务';
+
+create table sms_failed_task (
+  id int(10) unsigned not null auto_increment,
+  uid int(10) unsigned unique not null comment '用户UID',
+  sms_task_id int(10) unsigned not null comment '短信任务ID',
+  failed_reason varchar(100) default '' comment '失败原因',
+  created_at timestamp null default null,
+  updated_at timestamp null default null,
+  primary key (id)
+) engine=InnoDB default charset=utf8 comment='短信失败任务';
+
+create table sms_task_threshold_value (
+  id int(10) unsigned not null auto_increment,
+  uid int(10) unsigned unique not null comment '用户UID',
+  symbol_pari varchar(20) not null comment '价格对',
+  exchange tinyint(3) unsigned not null comment '交易所 [1.huobi 2.dragonex]',
+  threshold_value varchar(20) not null comment '阈值',
+  base_vale varchar(20) not null comment '基准值',
+  deviation tinyint(1) not null comment '偏离方向 [1.大于 2.小于]',
+  created_at timestamp null default null,
+  updated_at timestamp null default null,
+  primary key (id)
+) engine=InnoDB default charset=utf8 comment='短信阈值提醒任务';
