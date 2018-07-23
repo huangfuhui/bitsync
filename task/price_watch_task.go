2e9d7401d1a2f77c65fba401e874798d42cfd66c
huangfuhui <huangfuhui@outlook.com>
2018-07-23 11:59:59 +0800

    取消注释

--- task/price_watch_task.go
+++ task/price_watch_task.go
@@ -7,20 +7,20 @@ type PriceWatchTask struct {
 
 // 启动价格监控
 func (t *PriceWatchTask) Watch() {
-	// huobi := services.HuobiService{}
-	// go huobi.WatchHuobi()
-	//
-	// dragonex := services.DragonexService{}
-	// go dragonex.WatchDragonex()
-	//
-	// okex := services.OkexService{}
-	// go okex.WatchOkex()
-	//
-	// gate := services.GateService{}
-	// go gate.WatchGate()
-	//
-	// bithumb := services.BithumbService{}
-	// go bithumb.WatchBithumb()
+	huobi := services.HuobiService{}
+	go huobi.WatchHuobi()
+
+	dragonex := services.DragonexService{}
+	go dragonex.WatchDragonex()
+
+	okex := services.OkexService{}
+	go okex.WatchOkex()
+
+	gate := services.GateService{}
+	go gate.WatchGate()
+
+	bithumb := services.BithumbService{}
+	go bithumb.WatchBithumb()
 
 	binance := services.BinanceService{}
 	go binance.WatchBinance()
