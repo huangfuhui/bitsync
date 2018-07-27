06e31667f1a9c6a81575b3196d3cadc4fdbd7fbe
huangfuhui <huangfuhui@outlook.com>
2018-07-27 19:17:09 +0800

    添加常量

--- object/coin/exchange.go
+++ object/coin/exchange.go
@@ -5,7 +5,20 @@ import "bitsync/object"
 const (
 	EXCHANGE_HUOBI     = 1
 	EXCHANGE_DRAGEONEX = 2
+	EXCHANGE_OKEX      = 3
+	EXCHANGE_BINANCE   = 4
+	EXCHANGE_GATE      = 5
+	EXCHANGE_BITHUMB   = 6
 )
+
+var EXCHANGE = map[int]string{
+	1: "huobi",
+	2: "dragonex",
+	3: "okex",
+	4: "binance",
+	5: "gate",
+	6: "bithumb",
+}
 
 type Exchange struct {
 	object.Base
